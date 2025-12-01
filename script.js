fbdb7bd6b525d1daf6ebae56db81b6f5d5e2496e
anaclara724 <ana.claraandrade.ferreira@escola.pr.gov.br>
2025-12-01 11:19:51 -0300

    Add files via upload

---
+++ script.js
@@ -0,0 +1,26 @@
+const calcular = document.getElementById('calcular');
+
+function imc(){
+    const nome = document.getElementById('nome').value;
+    const altura = document.getElementById('altura').value;
+    const peso = document.getElementById('peso').value;
+    const resultado = document.getElementById
+    ('resultado');
+
+    const valorIMC = (peso/(altura*altura)).toFixed(1);
+
+    
+    let classificacao = "";
+
+    if (valorIMC < 18.5){
+        classificacao = "Abaixo do peso";
+    }else if (valorIMC < 25){
+        classificacao = "Peso ideal";
+    }else if (valorIMC > 30){
+        classificacao = "levimente acima do peso"
+    }else {
+        classificacao = "Obesidade"
+    }
+    resultado.textContent = `${nome} seu IMC é ${valorIMC} , sua classificacão é ${classificacao}`;
+}
+calcular.addEventListener('click',imc);
